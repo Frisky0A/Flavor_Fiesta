ea887666ccb92a55f1bfbc4a0ac22ef829d8e6ba
Adam <adamshawky222@gmail.com>
2024-02-18 05:02:22 +0200

    Finalyzing

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -3,10 +3,22 @@ import { GiKnifeFork } from "react-icons/gi";
 import { Button } from "..";
 import { Link } from "react-router-dom";
 
+
+// swiper 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Keyboard, Pagination, Navigation } from 'swiper/modules';
+
 const Hero = () => {
   return (
     <section className="box h-[82vh] flex flex-col-reverse md:flex-row justify-between gap-8 md:gap-16">
-      <div className="flex flex-col items-center md:items-start justify-center basis-1/2 gap-4">
+      {/* <div className="flex flex-col items-center md:items-start justify-center basis-1/2 gap-4">
         <span className="text-primary text-sm px-4 py-1 rounded-full border-2 border-primary max-w-max">
           Feast. Share. Connect.
         </span>
@@ -28,7 +40,30 @@ const Hero = () => {
           />
         </Link>
       </div>
-      <div className="basis-1/2 bg-hero bg-no-repeat bg-cover bg-center rounded-xl"></div>
+      <div className="basis-1/2 bg-hero bg-no-repeat bg-cover bg-center rounded-xl"></div> */}
+       <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        keyboard={{
+          enabled: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Keyboard, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
     </section>
   );
 };
